0370cc51948f2a4bd8b1a999381175d43501d1a0
Alejandro Zapata <alzapata317@gmail.com>
2022-12-30 21:46:27 -0500

    Added login and logout sessions and added user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
@@ -23,4 +23,6 @@ def create_app(test_config=None):
   app.jinja_env.filters['format_date'] = filters.format_date
   app.jinja_env.filters['format_plural'] = filters.format_plural
 
+  app.register_blueprint(api)
+  
   return app
